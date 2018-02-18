5887adacfcb1cf39bda1a8b71ba005542acf5de7
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-02-18 17:32:24 +0100

    Add test.txt and function reading files

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ fs.stat('./2.jpg', function(err, stats){
 	var statMode = new StatMode(stats)
 	console.log(statMode.toString());
 });
+
+fs.readFile('./test.txt', 'utf-8', function(err, data){
+	console.log(data);
+});
+
+fs.writeFile('./test.txt', ' Tekst dopisany', function(err){
+	if(err) throw err;
+	console.log('Zapisano');
+});
+
+console.log(fs);
