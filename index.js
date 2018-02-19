5296c7004fea6f4f59bd76bf75c511e3e08807a5
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-02-19 11:38:44 +0100

    Ready task

--- index.js
+++ index.js
@@ -1,51 +1,22 @@
 process.stdin.setEncoding('Utf-8');
 var fs = require('fs');
-var StatMode = require('stat-mode');
-var colors = require('colors');
-
-
-/*fs.stat('./2.jpg', function(err, stats){
-	var statMode = new StatMode(stats)
-	console.log(statMode.toString());
-});
-
-fs.readFile('./test.txt', 'utf-8', function(err, data){
-	console.log('Dane przed zapisem'.blue);
-	console.log(data);
-
-	fs.appendFile('./test.txt', '\n Niech moc będzie z tobą', function(err){
-		if(err) throw err;
-		console.log('Zapisano'.blue);
-		fs.readFile('./test.txt', 'utf-8',  function(err, data){
-			console.log('Dane po zapisie: '.blue);
-			console.log(data);
-		});
-	});	
-});*/
 
 fs.readdir('./.git', 'utf-8', function(err, files){
 	if(err) throw err;
 	var directoryContent = files;
-	console.log(directoryContent);
-	fs.writeFile('./gitdirectory.txt', files , 'utf-8',  function(err){
-		if(err) throw err;
-		fs.readFile('./gitdirectory.txt', 'utf-8',  function(err, data){
+		
+	for(var i=0; i<directoryContent.length; i++){
+		var position = '\n' + directoryContent[i];
+		console.log(position);
+		fs.appendFile('./gitdirectory1.txt', position , function(err){
 			if(err) throw err;
-			console.log('Zawartość katalogu .git: ',data);
-		});
-	});
-	
-	for(var i = 0; i >= directoryContent.length; i++){
-		var position = directoryContent[i];
-		fs.appendFile('./gitdirectory1.txt', 'bbb', function(err){
-			if(err) throw err;
-			console.log('zapisano');
 			});
 	}
-	
-fs.readFile('./gitdirectory1.txt', 'utf-8', function(err, data){
-	if(err) throw err;
-	console.log(data);
+	console.log('\nWriting the file done');
 });
 
+
+fs.writeFile('./gitdirectory1.txt', '', function(err, data){
+	if(err) throw err;
+	console.log('\nClearing the file done');
 });
